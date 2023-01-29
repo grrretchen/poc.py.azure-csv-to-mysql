26d78095d9ca81012cba5d41308427c8f0a909e0
Peter Keppel <pckeppel@gmail.com>
2023-01-29 10:15:04 -0600

    Handlers to retrieve CSV from URL

--- app_csv2mysql/main.py
+++ app_csv2mysql/main.py
@@ -17,6 +17,11 @@ class Main():
         pass
 
     # -------------------------------------------------------------------------
+    def main(self):
+        csv = self.getCSV()
+        print(csv)
+
+    # -------------------------------------------------------------------------
     def hello(self, name):
         """
         Method doscstring
@@ -28,9 +33,41 @@ class Main():
         logging.info(output)
         print(output)
 
+    # -------------------------------------------------------------------------
+    def getCSV(self):
+        """
+        Retrieve CSV from URL
 
+        Returns:
+            string: Raw CSV response, or FALSE
+        """
+        url = "https://raw.githubusercontent.com/shopifypartners/product-csvs/master/home-and-garden.csv"
+        resp = fetch(url)
+        if not resp:
+            return False
+        return resp
+
+
+# -----------------------------------------------------------------------------
+def fetch(url,headers={}):
+    """
+    Method: Basic wrapper for Requests. Provides boilerplate for error processing.
+
+    Args:
+        url (string): _description_
+        headers (dict, optional): Dictionary of HTTP headers. Defaults to {}.
+
+    Returns:
+        string: content of URL, or FALSE.
+    """
+    logging.info('Retrieving data from URL %s'%url)
+    resp = requests.get(url,headers=headers)
+    if not resp.status_code==200:
+        return False
+    return resp.text
+    
 # =============================================================================
 if __name__ == "__main__":
     MyMain = Main()
     MyMain.hello('World')
-    
+    MyMain.main()
